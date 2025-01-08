dbb2e5307ea7f64591a6f41945147a71be00d3ea
Muhd. Danial Danish Roslan <distantwaterstream@gmail.com>
2025-01-08 12:03:00 +0800

    debug: try#1

--- tina/collections/post.js
+++ tina/collections/post.js
@@ -17,6 +17,47 @@ export default {
       name: "body",
       isBody: true,
     },
+    {
+			name: 'featured_image',
+			label: 'Featured Image',
+			type: 'object',
+			fields: [
+				{
+					type: 'image',
+					name: 'imageURL',
+					label: 'Image URL',
+					ui: {
+						component: 'image-detailed',
+					},
+				},
+				{
+					type: 'string',
+					name: 'imageType',
+					label: 'Image Type',
+					ui: {
+						disabled: true,
+					},
+				},
+				{
+					type: 'number',
+					name: 'imageHeight',
+					label: 'Image Height (in px)',
+					ui: {
+						disabled: true,
+						component: 'text',
+					},
+				},
+				{
+					type: 'number',
+					name: 'imageWidth',
+					label: 'Image Width (in px)',
+					ui: {
+						disabled: true,
+						component: 'text',
+					},
+				},
+			],
+		},
   ],
   ui: {
     router: ({ document }) => {
